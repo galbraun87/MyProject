cd8910192a24372c5fb7c6773fc4f47a596c060d
galbraun87 <105608940+galbraun87@users.noreply.github.com>
2024-12-14 20:06:02 +0200

    Another try

--- Build/Build_Web.loader.js
+++ Build/Build_Web.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68074570,
+    wasmFileSize: 68074572,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
